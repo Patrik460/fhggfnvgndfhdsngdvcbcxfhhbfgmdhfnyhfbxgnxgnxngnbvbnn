235311c8e029d8c7735181b2082711bff3afc650
Patrik460 <161054668+Patrik460@users.noreply.github.com>
2024-03-01 12:07:39 +0100

    Update ShipSession.java
    s

--- sea/CompanyApp/ShipSession.java
+++ sea/CompanyApp/ShipSession.java
@@ -5,88 +5,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ShipSession extends Thread {
 
-	private CompanyApp companyApp;
-	private Socket toSeaTrade;
-	private final Map<String, Socket> connectedShips = Collections.synchronizedMap(new HashMap<>());
-	private BufferedReader inSession;
-	private PrintWriter outSession;
+    private CompanyApp companyApp;
+    private Socket toSeaTrade;
+    private BufferedReader inSession;
+    private PrintWriter outSession;
+    private String shipName;
 
-	public ShipSession(CompanyApp companyApp, Socket toSeaTrade) {
-		this.companyApp = companyApp;
-		this.toSeaTrade = toSeaTrade;
-		try {
-			this.inSession = new BufferedReader(new InputStreamReader(toSeaTrade.getInputStream()));
-			this.outSession = new PrintWriter(toSeaTrade.getOutputStream(), true);
-		} catch (IOException e) {
-			System.out.println("IOException in ShipSession constructor: " + e.getMessage());
-		}
-	}
+    public ShipSession(CompanyApp companyApp, Socket toSeaTrade) {
+        this.companyApp = companyApp;
+        this.toSeaTrade = toSeaTrade;
 
-	@Override
-	public void run() {
-		String shipName;
-		try {
-			shipName = inSession.readLine();
-			System.out.println("Ship " + shipName + " connected to Company " + companyApp.getCompName());
-			String message;
-			while ((message = inSession.readLine()) != null && !Thread.currentThread().isInterrupted()) {
-				System.out.println("Received message from " + shipName + ": " + message);
-				// Process message
-			}
-		} catch (IOException e) {
-			System.out.println("IOException in ShipSession run: " + e.getMessage());
-		} finally {
-			Close();
-			companyApp.remove(this);
-		}
-	}
+        try {
+            this.inSession = new BufferedReader(new InputStreamReader(toSeaTrade.getInputStream()));
+            this.outSession = new PrintWriter(toSeaTrade.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("IOException in ShipSession constructor: " + e.getMessage());
+        }
+    }
 
-	public synchronized void AddShip(Socket shipSocket) {
-		try {
-			BufferedReader shipReader = new BufferedReader(new InputStreamReader(shipSocket.getInputStream()));
-			String shipName = shipReader.readLine();
-			connectedShips.put(shipName, shipSocket);
-		} catch (IOException e) {
-			System.out.println("IOException in addShip: " + e.getMessage());
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            String message;
+            while ((message = inSession.readLine()) != null && !Thread.currentThread().isInterrupted()) {
+                System.out.println("Received message from " + shipName + ": " + message);
 
-	public void Send(String message) {
-		outSession.println(message);
-	}
+                // Process the incoming messages
+                processMessage(message);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException in ShipSession run: " + e.getMessage());
+        } finally {
+            Close();
+        }
+    }
 
-	// TODO: Zur ShipApp auslagern
-	public void RegisterToCompany(String companyName) {
-		companyApp.addShipSession(this);
-	}
+    private void processMessage(String message) {
+        if (message.startsWith("login:")) {
+            // Extract shipName from the login message
+            shipName = message.substring("login:".length());
+            System.out.println("Ship " + shipName + " connected to Company " + companyApp.getCompName());
+            companyApp.addShipSession(this);
+        } else if (message.startsWith("logout:")) {
+            // Extract shipName from the logout message
+            shipName = message.substring("logout:".length());
+            System.out.println("Ship " + shipName + " logged out from Company " + companyApp.getCompName());
+            Close(); // Close the ShipSession
+        } else {
+            // Process other messages as needed
+            // ...
+        }
+    }
 
-	// TODO: Zur ShipApp auslagern
-	public void UnregisterFromCompany() {
-		companyApp.remove(this);
-	}
+    public void Send(String message) {
+        outSession.println(message);
+    }
 
-	public synchronized void Close() {
-		try {
-			if (toSeaTrade != null) {
-				toSeaTrade.close();
-			}
-			// Weitere Aufräumarbeiten
-		} catch (IOException e) {
-			System.out.println("IOException in close: " + e.getMessage());
-		}
-	}
+    public synchronized void Close() {
+        try {
+            if (toSeaTrade != null) {
+                toSeaTrade.close();
+            }
+            // Additional cleanup tasks
+        } catch (IOException e) {
+            System.out.println("IOException in close: " + e.getMessage());
+        }
+    }
 
-	public Map<String, Socket> getConnectedShips() {
-		return connectedShips;
-	}
-
-	public Socket GetSocket() {
-		return toSeaTrade;
-	}
+    public String getShipName() {
+        return shipName;
+    }
 }
