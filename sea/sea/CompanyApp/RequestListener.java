320ef04214940c158445073a1b45e252d6e888c8
Patrik460 <161054668+Patrik460@users.noreply.github.com>
2024-03-05 14:08:00 +0100

    SSocket

---
+++ sea/sea/CompanyApp/RequestListener.java
@@ -0,0 +1,278 @@
+package sea.CompanyApp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class RequestListener extends Thread {
+
+	private final int port;
+	private CompanyApp companyApp;
+	private PrintWriter out;
+
+	public RequestListener(int port) {
+		this.port = port;
+	}
+
+	public void setCompanyApp(CompanyApp companyApp) {
+		this.companyApp = companyApp;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("Versuch Server");
+
+			ServerSocket sSocket = new ServerSocket(port);
+			System.out.println("Server started. Waiting for connection...");
+
+			while (!Thread.currentThread().isInterrupted()) {
+				Socket cSocket = sSocket.accept();
+				System.out.println("Connected to: " + cSocket.getInetAddress());
+				OutputStream toClient = cSocket.getOutputStream();
+				PrintWriter writer = new PrintWriter(toClient);
+				processInput("sendCost");
+
+			}
+
+		} catch (IOException e) {
+			System.out.println("IOException in RequestListener: " + e.getMessage());
+		} finally {
+			closeServerSocket();
+		}
+	}
+
+	public void processInput(String inputLine) {
+		String command = getCommand(inputLine);
+		String value = getValue(inputLine);
+
+		switch (command) {
+		case "login": // login:AIDA
+			// TODO: Login und Logout gehören in ShipSession login(value);
+			break;
+		case "sendPos": // sendPos:12,18
+			sendPos(value);
+			break;
+		case "sendDir": // sendDir:NORTH
+			sendDir(value);
+			break;
+		case "sendProfit": // sendProfit:20000
+			sendProfit(value);
+			break;
+		case "sendCost":
+			companyApp.setBalance(349);
+			sendCost(value);
+			break;
+		case "logout": // logout:AIDA
+			// TODO: Login und Logout gehören in ShipSession
+			logoutFromCompany(value);
+			break;
+		case "receiveOrder": // receiveOrder:AIDA ??
+			receiveOrder(value);
+			break;
+		case "endOrder": // endOrder:19 ??
+			endOrder(value);
+			break;
+		case "loadCargo": // loadCargo:19
+			loadCargo(value);
+			break;
+		case "unloadCargo": // unloadCargo:19
+			unloadCargo(value);
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void sendCost(String value) {
+		String companyName = companyApp.getCompName();
+		String sql = "UPDATE company SET Balance = Balance - " + value + " WHERE Name = " + companyName;
+		Connection connection;
+		try {
+			connection = connectToDatabase();
+			updateCompanyBalance(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+//TODO: Login und Logout gehören in ShipSession
+	/*
+	 * private void login(String shipName) { out.println(shipName +
+	 * " has logged in."); ShipSession shipSession = new
+	 * ShipSession(companyApp.getCompanyApp(), cSocket);
+	 * shipSession.AddShip(shipSession.GetSocket()); }
+	 */
+
+	private void sendProfit(String value) {
+		String companyName = companyApp.getCompName();
+		String sql = "UPDATE company SET Balance = Balance + " + value + " WHERE Name = " + companyName;
+		Connection connection;
+		try {
+			connection = connectToDatabase();
+			updateCompanyBalance(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendDir(String value) {
+		String sql = "UPDATE ship SET Direction = '" + value + "' WHERE Name = '" + companyApp.getCompName() + "'";
+		Connection connection;
+		try {
+			connection = connectToDatabase();
+			updateShipDirection(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendPos(String value) {
+		String[] positionValues = value.split(",");
+		String PosX = positionValues[0].trim();
+		String PosY = positionValues[1].trim();
+		String sql = "UPDATE ship SET PosX = " + PosX + ", PosY = " + PosY + " WHERE Name = "
+				+ companyApp.getCompName();
+		Connection connection;
+		try {
+			connection = connectToDatabase();
+			updateShipPosition(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getCommand(String inputLine) {
+		String[] splitCommand = inputLine.split(":");
+		return (splitCommand.length > 0) ? splitCommand[0] : "";
+	}
+
+	private String getValue(String inputLine) {
+		String[] splitCommand = inputLine.split(":");
+		return (splitCommand.length > 1) ? splitCommand[1] : "";
+	}
+
+	private void logoutFromCompany(String shipName) {
+		out.println(shipName + " has logged out.");
+		companyApp.addShipSession(null);
+	}
+
+	private void receiveOrder(String shipName) {
+		String message = shipName + " has accepted the Order.";
+		displayMessageWindow(message);
+	}
+
+	private void endOrder(String value) {
+		String message = "Cargo " + value + " has been successfully shipped!";
+		displayMessageWindow(message);
+	}
+
+	private void loadCargo(String cargoID) {
+		String sql = "UPDATE cargo SET Status = 1 WHERE CargoID = " + cargoID;
+		Connection connection;
+
+		try {
+			connection = connectToDatabase();
+			updateCargoStatus(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void unloadCargo(String cargoID) {
+		String sql = "UPDATE cargo SET Status = 2 WHERE CargoID = " + cargoID;
+		Connection connection;
+
+		try {
+			connection = connectToDatabase();
+			updateCargoStatus(connection, sql);
+			closeDatabaseConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Connection connectToDatabase() throws SQLException {
+		String jdbcUrl = "jdbc:mysql://localhost:3305/seatradedb";
+		String username = "root";
+		String password = "";
+
+		return DriverManager.getConnection(jdbcUrl, username, password);
+	}
+
+	private void closeDatabaseConnection(Connection connection) {
+		try {
+			if (connection != null && !connection.isClosed()) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("Error closing database connection: " + e.getMessage());
+		}
+	}
+
+	private void updateCompanyBalance(Connection connection, String sql) {
+		try {
+			try (Statement statement = connection.createStatement()) {
+				statement.executeUpdate(sql);
+				out.println("Company balance updated");
+			}
+		} catch (SQLException e) {
+			System.out.println("Error updating company balance: " + e.getMessage());
+		}
+	}
+
+	private void updateCargoStatus(Connection connection, String sql) {
+		try {
+			try (Statement statement = connection.createStatement()) {
+				statement.executeUpdate(sql);
+				out.println("Cargo status updated");
+			}
+		} catch (SQLException e) {
+			System.out.println("Error updating cargo status: " + e.getMessage());
+		}
+	}
+
+	private void updateShipDirection(Connection connection, String sql) {
+		try {
+			try (Statement statement = connection.createStatement()) {
+				statement.executeUpdate(sql);
+				out.println("Ship direction updated");
+			}
+		} catch (SQLException e) {
+			System.out.println("Error updating ship direction: " + e.getMessage());
+		}
+	}
+
+	private void updateShipPosition(Connection connection, String sql) {
+		try {
+			try (Statement statement = connection.createStatement()) {
+				statement.executeUpdate(sql);
+				out.println("Ship position updated");
+			}
+		} catch (SQLException e) {
+			System.out.println("Error updating ship position: " + e.getMessage());
+		}
+	}
+
+	private void displayMessageWindow(String message) {
+		JOptionPane.showMessageDialog(null, message, "Instant Message", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public void closeServerSocket() {
+		// TODO
+	}
+}
---
+++ sea/sea/Erweiterungen/GUI/CompanyAppGUI.java
@@ -0,0 +1,213 @@
+package sea.Erweiterungen.GUI;
+
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+import sea.CompanyApp.CompanyApp;
+import sea.CompanyApp.Receiver;
+
+public class CompanyAppGUI {
+
+	private JFrame frame;
+	private final CompanyApp companyApp;
+	private Receiver receiver;
+	private JPanel panel;
+	private JTextField companyNameField;
+	private JLabel updatedBalanceLabel, newBalanceLabel;
+	private JComboBox<String> cargoDropdown, shipDropdown;
+	private Timer balanceUpdateTimer;
+
+	public CompanyAppGUI() {
+		this.companyApp = new CompanyApp();
+		companyApp.setReceiver(new Receiver());
+		setReceiver(companyApp.getReceiver());
+
+		initializeGUI();
+	}
+
+	public static void main(String[] args) {
+		new CompanyAppGUI();
+	}
+
+	private void initializeGUI() {
+		frame = new JFrame("SeaTrade Company App");
+		frame.setSize(450, 330);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		panel = new JPanel();
+
+		frame.getContentPane().add(panel); // ??
+
+		placeComponents();
+
+		AllButtonEnabled(false);
+		ButtonsEnabled(true, "Register");
+
+		frame.setVisible(true);
+	}
+
+	private void placeComponents() {
+		panel.setLayout(null);
+
+		// Labels und Textfeld für Name und Register-Button
+		addLabel(panel, "Name:", 10, 20, 80, 25);
+		companyNameField = addTextField(panel, 80, 20, 160, 25);
+		addButton(panel, "Register", 250, 20, 160, 25, e -> registerCompany());
+
+		// Labels und Buttons für Balance, Ship list, Harbours, Cargos
+		addLabel(panel, "Balance:", 10, 60, 80, 25);
+		newBalanceLabel = addLabel(panel, companyApp.getBalance() + "", 80, 60, 160, 25);
+		addButton(panel, "Add Ship", 250, 60, 160, 25, e -> {
+			new ShipAppGUI();
+		});
+		addButton(panel, "Harbours", 80, 100, 160, 25, e -> showHarbourInfo());
+		addButton(panel, "Cargos", 250, 100, 160, 25, e -> showCargoInfo());
+
+		String[] shipOptions = { "Ship1", "Ship2", "Ship3" };
+		shipDropdown = addComboBox(panel, shipOptions, 80, 180, 160, 25);
+
+		// Button für Order und Label für Cargo
+		addButton(panel, "Order", 250, 180, 160, 65, e -> orderShip());
+		addLabel(panel, "Cargo:", 10, 140, 80, 25);
+
+		// Exit Button
+		addButton(panel, "Exit", 250, 260, 160, 25, e -> exitApplication());
+
+		balanceUpdateTimer = new Timer(500, e -> updateBalanceLabel());
+		balanceUpdateTimer.start();
+	}
+
+	private void updateBalanceLabel() {
+		newBalanceLabel.setText(companyApp.getBalance() + "");
+	}
+
+	// Helfermethoden
+	private JLabel addLabel(JPanel panel, String text, int x, int y, int width, int height) {
+		JLabel label = new JLabel(text);
+		label.setBounds(x, y, width, height);
+		panel.add(label);
+		return label;
+	}
+
+	private JTextField addTextField(JPanel panel, int x, int y, int width, int height) {
+		JTextField textField = new JTextField();
+		textField.setBounds(x, y, width, height);
+		panel.add(textField);
+		return textField;
+	}
+
+	private JComboBox<String> addComboBox(JPanel panel, String[] options, int x, int y, int width, int height) {
+		JComboBox<String> comboBox = new JComboBox<>(options);
+		comboBox.setBounds(x, y, width, height);
+		panel.add(comboBox);
+		return comboBox;
+	}
+
+	private JButton addButton(JPanel panel, String text, int x, int y, int width, int height,
+			ActionListener actionListener) {
+		JButton button = new JButton(text);
+		button.setBounds(x, y, width, height);
+		button.addActionListener(actionListener);
+		panel.add(button);
+		return button;
+	}
+
+	public void AllButtonEnabled(boolean result) {
+		Component[] components = panel.getComponents();
+		for (Component component : components) {
+			if (component instanceof JButton) {
+				component.setEnabled(result);
+			}
+		}
+	}
+
+	public void ButtonsEnabled(boolean result, String button) {
+		String[] buttons = new String[] { button }; // Standardwert
+		if (button.contains(",")) {
+			buttons = button.split(",");
+		}
+		Component[] components = panel.getComponents();
+		for (Component component : components) {
+			if (component instanceof JButton currentButton) {
+				for (String btn : buttons) {
+					if (currentButton.getText().equals(btn.trim())) {
+						currentButton.setEnabled(result);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	private void registerCompany() {
+		String CompanyName = companyNameField.getText().trim();
+		if (!CompanyName.isEmpty() && companyApp.getReceiver() != null) {
+			if (!companyApp.getReceiver().isAlive()) {
+				companyApp.getReceiver().start();
+				try {
+					Thread.sleep(1000);
+					companyApp.getReceiver().out.println("register:" + companyNameField.getText());
+
+					AllButtonEnabled(true);
+					ButtonsEnabled(false, "Register");
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void updateBalance(String newBalance) {
+		updatedBalanceLabel.setText(newBalance);
+	}
+
+	private void showHarbourInfo() {
+		companyApp.getHarbourInfo();
+
+	}
+
+	private void showCargoInfo() {
+		// Die Methode getCargoInfo() muss die Cargos aktualisieren
+		companyApp.getCargoInfo();
+
+		String[] aa = receiver.getReceivedCargos();
+
+		if (cargoDropdown != null) {
+			panel.remove(cargoDropdown); // Entferne die vorhandene JComboBox
+		}
+		cargoDropdown = addComboBox(panel, aa, 80, 140, 330, 25); // Erstelle die JComboBox neu
+		panel.revalidate(); // Aktualisiere das Panel, um die Änderungen anzuzeigen
+		panel.repaint();
+	}
+
+	private void orderShip() {
+		updateBalanceLabel();
+		companyApp.distributeOrder();
+	}
+
+	private void sendCommand() {
+		// TODO
+	}
+
+	private void exitApplication() {
+		balanceUpdateTimer.stop();
+		frame.dispose(); // Schließe das Fenster
+		System.exit(0); // Beende die Anwendung
+	}
+
+	public Receiver getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(Receiver receiver) {
+		this.receiver = receiver;
+	}
+
+}
